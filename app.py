032371fba1a260d3b6badf3b940dfd0f27b4c236
Roni Rigoni <ronirigoni@gmail.com>
2022-09-22 20:37:02 -0300

    Heroku app v1.0

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from flask import Flask
-import numpy as np
-import pandas as pd
-import re
-from nltk import sent_tokenize, word_tokenize
-from nltk.stem.snowball import SnowballStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
