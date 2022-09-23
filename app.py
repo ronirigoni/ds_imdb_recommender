22fc6c0bd5dc841a034b25ea2b2b09bc3d1063a9
Roni Rigoni <ronirigoni@gmail.com>
2022-09-22 21:34:41 -0300

    Heroku app v1.0

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import nltk
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def home():
+
+    fidubil = request.get("fi")
+    if fidubil:
+        return "Bora fiii du Billl " + fidubil
+        
     return "Booora Biiiill!!!"
     
 if __name__ == "__main__":
